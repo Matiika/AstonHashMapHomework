20d09e8b5e7bbecc1ebf9d81e4b8b95c5a73042e
Матвей <matvei@MacBook-Pro-Matvej.local>
2025-05-13 23:50:27 +0300

    new commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyHashMap myHashMap = new MyHashMap<>();
+        myHashMap.put(1, "Mat");
+        myHashMap.put(2, "Tru");
+        System.out.println(myHashMap.get(1));
+        System.out.println(myHashMap.get(2));
+
+        myHashMap.put(1, "Han");
+        System.out.println(myHashMap.get(1));
+
     }
 }
---
+++ src/main/java/org/example/MyHashMap.java
@@ -0,0 +1,112 @@
+package org.example;
+
+import java.util.Map;
+import java.util.Objects;
+
+public class MyHashMap<K, V> {
+    // Массив корзин (buckets)
+    private Node<K,V>[] table;
+
+    // Текущий размер хеш-таблицы
+    private int size;
+
+    // Начальная емкость таблицы
+    private static final int DEFAULT_CAPACITY = 16;
+
+    @SuppressWarnings("unchecked")
+    public MyHashMap() {
+        table = (Node<K,V>[]) new Node[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+
+    static class Node<K,V> {
+        final int hash;
+        final K key;
+        V value;
+        MyHashMap.Node<K,V> next;
+
+        Node(int hash, K key, V value, MyHashMap.Node<K,V> next) {
+            this.hash = hash;
+            this.key = key;
+            this.value = value;
+            this.next = next;
+        }
+
+    }
+
+    public V put(K key, V value) {
+        //Считаем хэшкод и номер ячейки для вставки
+        int hash = (key == null) ? 0 : key.hashCode();
+        int index = hash & (table.length - 1);
+        Node<K, V> newNode = new Node<>(hash, key, value, null);
+
+        var currentNode = table[index];
+
+        //Если в ячейке ничего нет, то вставляем в нее
+        if (currentNode == null) {
+            table[index] = newNode;
+            size++;
+            return null;
+        }
+
+        while (currentNode != null) {
+
+            //если текущий Node имеет такой же ключ, то переписать значение. Отправить обратно старое значение.
+            if (currentNode.hash == hash && Objects.equals(currentNode.key, key)) {
+                var oldValue = currentNode.value;
+                currentNode.value = value;
+                return oldValue;
+            }
+
+            //Если ссылка на следующий Node равна null, то выйти из цикла
+            if (currentNode.next == null) {
+                break;
+            }
+            currentNode = currentNode.next;
+        }
+
+        currentNode.next = newNode;
+        size++;
+        return null;
+    }
+
+    public V get(K key) {
+        int hash = (key == null) ? 0 : key.hashCode();
+        int index = hash & (table.length - 1);
+
+        var currentNode = table[index];
+
+        while (currentNode != null) {
+            if (currentNode.hash == hash && (Objects.equals(currentNode.key, key))) {
+                return currentNode.value;
+            }
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+    public V remove(K key) {
+        int hash = (key == null) ? 0 : key.hashCode();
+        int index = hash & (table.length - 1);
+
+        Node<K, V> currentNode = table[index];
+        Node<K, V> prev = null;
+
+        while (currentNode != null) {
+            if (currentNode.hash == hash && (Objects.equals(currentNode.key, key))) {
+                if (prev == null) {
+                    table[index] = currentNode.next;
+                } else {
+                    prev.next = currentNode.next;
+                }
+                size--;
+                return currentNode.value;
+            }
+            prev = currentNode;
+            currentNode = currentNode.next;
+        }
+        return null;
+    }
+
+}
